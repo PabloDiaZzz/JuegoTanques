a13e03771d97c1583a51fc0ffafb7701674e6162
Pablo Diaz <gjv64177@educastur.es>
2026-02-27 11:23:33 +0100

    Añadido jugador extra

--- src/game/managers/PlayerManager.ts
+++ src/game/managers/PlayerManager.ts
@@ -13,6 +13,7 @@ export default class PlayerManager {
 
         players.push(new Tank(this.scene, 200, 0xff0000, 'Jugador Rojo'));
         players.push(new Tank(this.scene, 900, 0x00ff00, 'Jugador Verde'));
+        players.push(new Tank(this.scene, 1200, 0x0000ff, 'Jugador Azul'));
 
         return players;
     }
